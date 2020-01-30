033412e2e5c9e49104c6faf55f495f90d58a4032
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-01-30 17:33:49 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def collection(array)
+i=0
+while i<array.length
+yield array[i]
+i += 1
 end
+array
+end
+
+array=[1,2,3,4,5]
+
+collection(array) {|n| puts n}
